6919c677d624d7b4f877d5a31c0c3cde36bc3ed8
Chris Cankov <cankov.chris@gmail.com>
2017-04-13 13:53:42 -0700

    Model associations and validations

--- polls_app/PollApp/app/models/response.rb
+++ polls_app/PollApp/app/models/response.rb
@@ -1,2 +1,14 @@
 class Response < ApplicationRecord
+  validates :answer_id, presence: true
+  validates :user_id, presence: true
+
+  belongs_to :user,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+
+  belongs_to :answer_choice,
+    primary_key: :id,
+    foreign_key: :answer_id,
+    class_name: :AnswerChoice 
 end
